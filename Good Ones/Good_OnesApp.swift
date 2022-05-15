6a9d49f40799861982bd9e3ed7971f5e8a487324
Ráfagan Abreu <rafagan@kolbapps.com>
2022-05-15 09:06:14 -0300

    Initial Commit

---
+++ Good Ones/Good_OnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_OnesApp.swift
+//  Good Ones
+//
+//  Created by Ráfagan Abreu on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Good_OnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
